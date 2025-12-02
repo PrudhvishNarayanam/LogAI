646ab4477f2b82a32c79ee5258e9498d5d74300b
PrudhvishNarayanam <prudhvish.narayanam@accenture.com>
2025-12-02 18:27:49 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/logaisampleandroidapp/MainActivity.java
+++ app/src/main/java/com/example/logaisampleandroidapp/MainActivity.java
@@ -14,33 +14,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    private void logExceptionToFile(Exception e) {
-        try {
-            File logFile = new File(getFilesDir(), "app_exception_log.txt");
-            FileWriter writer = new FileWriter(logFile, true);
-            writer.write("Exception: " + e.toString() + "\n");
-            for (StackTraceElement element : e.getStackTrace()) {
-                writer.write("    at " + element.toString() + "\n");
-            }
-            writer.write("\n");
-            writer.close();
-        } catch (IOException ioException) {
-            Log.e("MainActivity", "Failed to write log file", ioException);
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        try {
-            // Intentionally cause a NullPointerException
-            String nullString = null;
-            int length = nullString.length(); // This will throw NullPointerException
-        } catch (Exception e) {
-            logExceptionToFile(e);
-        }
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
